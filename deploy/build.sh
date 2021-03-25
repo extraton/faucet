719515d9efd50388a45e1cd1da09b9049b279e8f
qwertys318 <343604@gmail.com>
2021-03-26 01:22:24 +0700

    deploy

---
+++ deploy/build.sh
@@ -0,0 +1,18 @@
+set -e
+
+dir=$(pwd)
+
+rm -rf ${dir}/build
+git clone https://github.com/extraton/faucet.git ${dir}/build
+cd ${dir}/build
+git checkout "${1}"
+cd front
+#yarn install
+#yarn run build
+cd ${dir}/build
+#cp -R ${dir}/build/front/dist/* ${dir}/build/public/
+rm -rf ./{.git,front}
+docker build -f ../deploy/Dockerfile -t docker.pkg.github.com/extraton/faucet/faucet-worker:${1} .
+#docker push docker.pkg.github.com/extraton/faucet/faucet-worker:${1}
+cd ../
+rm -rf ${dir}/build
