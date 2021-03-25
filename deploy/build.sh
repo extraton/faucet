acd436b2dc5af00e3ce65c7c513babaf34e87084
qwertys318 <343604@gmail.com>
2021-03-26 01:56:47 +0700

    fix deploy

--- deploy/build.sh
+++ deploy/build.sh
@@ -7,12 +7,12 @@ git clone https://github.com/extraton/faucet.git ${dir}/build
 cd ${dir}/build
 git checkout "${1}"
 cd front
-#yarn install
-#yarn run build
+yarn install
+yarn run build
 cd ${dir}/build
-#cp -R ${dir}/build/front/dist/* ${dir}/build/public/
+cp -R ${dir}/build/front/dist/* ${dir}/build/public/
 rm -rf ./{.git,front}
 docker build -f ../deploy/Dockerfile -t docker.pkg.github.com/extraton/faucet/faucet-worker:${1} .
-#docker push docker.pkg.github.com/extraton/faucet/faucet-worker:${1}
+docker push docker.pkg.github.com/extraton/faucet/faucet-worker:${1}
 cd ../
 rm -rf ${dir}/build
